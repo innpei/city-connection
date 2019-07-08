3f1e820d74bbb24deb8daf3463da06baa2cfdbf6
innpei <43521839+innpei@users.noreply.github.com>
2019-07-08 09:38:47 +0300

    Update graph.h

--- graph.h
+++ graph.h
@@ -27,17 +27,9 @@ class Graph
 public:
     int V, E;
     Edge* edge;
-    //vector<Node*> vecOfNodes;
-
-    //vector<pair<string, vector<double>>> SaveData();
-    //vector<vector<double>> distance_mat();
-    //Graph* createGraph(int v,int e);
-
-
 };
 
 //----------------------------------------------------------------------------------------------------------------------
-/*This part is definition of methods defined above*/
 
 ///save data from csv file
 vector<pair<string, vector<double>>> SaveData()
@@ -65,47 +57,13 @@ vector<pair<string, vector<double>>> SaveData()
         coordinates.push_back(dolgota);
         p = make_pair(city, coordinates);
         all_info.push_back(p);
-        //cout << city << ' ' << shirota << ' ' << dolgota;
     }
-
-//    for(auto a: all_info)
-//        cout << a.first << ' ';
-//    cout << endl;
 
     return all_info;
 }
 
 vector<pair<string, vector<double>>> info = SaveData();
 
-///create distance matrix
-vector<vector<double>> distance_mat() {
-
-    vector<vector<double>> dist_matrix;
-    for(int j=0; j<SaveData().size();++j)
-    {
-        vector<double> v;
-        for(int i=0; i<SaveData().size();++i)
-        {
-            double distance;
-            if (j==i)
-                distance = 0;
-            distance = sqrt(pow(SaveData()[i].second[0]-SaveData()[j].second[0],2)+pow(SaveData()[i].second[1]-SaveData()[j].second[1],2));
-            v.push_back(distance);
-        }
-        dist_matrix.push_back(v);
-    }
-
-//    for(int i=0; i<dist_matrix.size();++i)
-//    {
-//        for (int j=0; j<dist_matrix[0].size();++j)
-//        {
-//            cout << dist_matrix[i][j] << ' ';
-//        }
-//        cout << endl;
-//    }
-
-    return dist_matrix;
-}
 
 Graph* createGraph(int v,int e)
 {
